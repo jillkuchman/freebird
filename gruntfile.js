79a00a664545cfee64511efd2df27f02291dc968
Jill Kuchman <jillkuchman@gmail.com>
2015-07-20 11:52:41 -0700

    Change basic colors in variables.less, add gruntfile for LESS processing

---
+++ gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+  
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "css/styles.css": "less/freelancer.less" // destination file and source file
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['less/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'watch']);
+};
